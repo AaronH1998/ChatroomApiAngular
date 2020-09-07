a04711623ea33b060bd5819f355837d9d9611e2f
AaronHodgson <aaronhodgson1998@gmail.com>
2020-09-07 15:43:41 +0100

    login page added

--- ChatroomFrontEnd/src/app/app.component.ts
+++ ChatroomFrontEnd/src/app/app.component.ts
@@ -1,27 +1,10 @@
-import { Component,OnInit } from '@angular/core';
-import { $ } from 'protractor';
-import { HttpClient } from '@angular/common/http';
-import{Message} from './message';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  title:'blah';
-  messages: Message[];
-  constructor(private http:HttpClient){}
-
-  ngOnInit():void{
-    this.getMessages();
-  }
-
-  getMessages():void{
-    this.http.get<Message[]>("https://localhost:44391/api/Chatroom").subscribe(messages => {
-      console.log(messages);
-      this.messages = messages;
-      console.log(this.messages);
-    });
-  }
+export class AppComponent {
+  title="Purple Tuesday Chatroom"
 }
