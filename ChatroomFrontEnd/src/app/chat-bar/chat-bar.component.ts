d31db42560e593db22667976e2459562e0d57549
AaronHodgson <aaronhodgson1998@gmail.com>
2020-09-08 16:20:30 +0100

    logic for scrollbar own messages and dates

--- ChatroomFrontEnd/src/app/chat-bar/chat-bar.component.ts
+++ ChatroomFrontEnd/src/app/chat-bar/chat-bar.component.ts
@@ -2,6 +2,8 @@ import { Component, EventEmitter, OnInit, Output, Input } from '@angular/core';
 import { Message } from '../message';
 import { MessageService } from '../message.service';
 import{ActivatedRoute} from '@angular/router';
+import * as $ from 'jquery';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-chat-bar',
@@ -12,19 +14,22 @@ export class ChatBarComponent implements OnInit {
   
   constructor(private route:ActivatedRoute, private messageService: MessageService) { }
 
-ngOnInit(): void {
+  ngOnInit(): void {
   }
   send(message: string): void {
       message = message.trim();
+
       if (!message) return;
-      console.log(this.route.paramMap);
-      let username = this.route.snapshot.paramMap.get("username");
+      
+      $("#messageInput").val('');
+
       let messageDetails = {
         Id:0,
         ChatMessage: message,
-        Username: username,
-        PostDate: (new Date()).toISOString()
+        Username: this.route.snapshot.paramMap.get("username"),
+        PostDate: moment().format()
       }
+
       this.messageService.sendMessage(messageDetails).subscribe(message => console.log(message));
   }
 }
--- ChatroomFrontEnd/src/app/messages/messages.component.ts
+++ ChatroomFrontEnd/src/app/messages/messages.component.ts
@@ -1,22 +1,49 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit,  ViewChildren, QueryList, AfterViewChecked, AfterViewInit } from '@angular/core';
 import { Message } from '../message';
 import { MessageService } from '../message.service';
+import{ActivatedRoute} from '@angular/router';
+import * as moment from 'moment';
+import { ObjectUnsubscribedError } from 'rxjs';
 
 @Component({
   selector: 'app-messages',
   templateUrl: './messages.component.html',
   styleUrls: ['./messages.component.css']
 })
-export class MessagesComponent implements OnInit {
-  @Input() event:any;
+export class MessagesComponent implements OnInit,AfterViewChecked {
+  @ViewChildren('scrollbar') scrollbar:QueryList<any>;
   messages:Message[];
-  constructor(private messageService:MessageService) { }
+  currentUsername:string;
+  entryTime:string;
+  moment: any = moment;
+  oldMessages: Message[];
+
+  constructor(private route:ActivatedRoute,private messageService:MessageService) { }
 
   ngOnInit(): void {
+    this.currentUsername = this.route.snapshot.paramMap.get("username");
+    this.entryTime = moment().format();
+    this.getMessages();
     setInterval(()=>this.getMessages(),1000);
   }
 
+  ngAfterViewChecked():void{
+    if(!this.oldMessages)
+      this.scrollToBottom();
+  }
+
   getMessages():void{
-    this.messageService.getMessages().subscribe(messages =>this.messages = messages)
+    if(this.messages){
+      this.oldMessages = JSON.parse(JSON.stringify(this.messages));
+    }
+    this.messageService.getMessages().subscribe(messages =>{
+      if(JSON.stringify(messages) != JSON.stringify(this.oldMessages)){
+        this.messages = messages; 
+      }
+    });
+  }
+  
+  scrollToBottom():void{
+    this.scrollbar.first.nativeElement.scrollTop = this.scrollbar.first.nativeElement.scrollHeight;
   }
 }
