ba44a66fa7b7aab43edf5d11c28548b0716536ce
AaronHodgson <aaronhodgson1998@gmail.com>
2020-09-09 10:48:45 +0100

    users controller added

---
+++ ChatroomApi/ChatroomApi/Controllers/UsersController.cs
@@ -0,0 +1,38 @@
+﻿using ChatroomApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ChatroomApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        PurpleTuesdayChatroomContext _context;
+        public UsersController(PurpleTuesdayChatroomContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public List<User> GetUsers()
+        {
+            return _context.Users.ToList();
+        }
+        [HttpPost]
+        public IActionResult AddUser(User user)
+        {
+            _context.Add(user);
+            _context.SaveChanges();
+            return Ok();
+        }
+        [HttpDelete("{username}")]
+        public IActionResult RemoveUser(string username)
+        {
+            _context.Remove(_context.Users.First(u => u.Username == username));
+            _context.SaveChanges();
+            return Ok();
+        }
+
+    }
+}
